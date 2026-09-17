bd5a21550d92fcccce11e5b4c070daa698141eea
saurabhsharmaj <sourabh.sharma0185@gmail.com>
2026-09-17 12:27:22 +0530

    update

--- start.bat
+++ start.bat
@@ -6,10 +6,22 @@ rmdir /s /q .camel-jbang-run 2>nul
 set "JAVA_FILES="
 
 for /r "src\main\java" %%F in (*.java) do (
-    set "JAVA_FILES=!JAVA_FILES! "%%F""
+
+    rem Check if Java file contains main() function
+    findstr /r /c:"main[ ]*(" "%%F" >nul
+
+    if errorlevel 1 (
+        rem No main() found - include the file
+        set "JAVA_FILES=!JAVA_FILES! "%%F""
+        echo INCLUDED: %%F
+    ) else (
+        rem main() found - skip this file and continue with next file
+        echo SKIPPED:  %%F
+    )
 )
 
-echo Java files:
+echo.
+echo Java files to run:
 echo !JAVA_FILES!
 echo.
 
